cf7f9338940725ddd6f3f752e181e346f52e9087
Nikolai <Nikolai.Balantsev@yandex.ru>
2023-10-05 01:41:34 +0300

    Add react-navigation, navigation header

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { Colors } from './constants/colors';
+import LinearGradient from 'react-native-linear-gradient';
+import { Pressable, StatusBar, StyleSheet } from 'react-native';
+import ChatsPage from './static/ChatsPage';
+import SearchSvg from './assets/search.svg';
+
+const Stack = createStackNavigator();
+
+const getHeaderBackground = () => (
+  <>
+    <LinearGradient style={styles.linearGradient} colors={[Colors.primary, '#B36BEB']} useAngle={true} angle={30} />
+    <StatusBar translucent={true} backgroundColor={'transparent'} barStyle={'light-content'} />
+  </>
+);
+
+const getSearchButton = () => (
+  <Pressable onPress={() => console.log(1233)} style={styles.searchButton}>
+    <SearchSvg width={22} height="100%" />
+  </Pressable>
+);
 
 function App() {
-  return <Text>Hello</Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackground: getHeaderBackground,
+          headerTintColor: Colors.secondary,
+          headerTitleAlign: 'center',
+          headerRight: getSearchButton,
+          cardStyle: { backgroundColor: '#FFF' },
+        }}>
+        <Stack.Screen name="Chats" component={ChatsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+  searchButton: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
 
 export default App;
--- app/constants/colors.ts
+++ app/constants/colors.ts
@@ -1,4 +1,7 @@
 export const Colors = {
   primary: '#6585E8',
   secondary: '#FFF',
+  grey: '#9a9a9a',
+  darkGrey: '#646464',
+  lightGrey: '#E3E3E3',
 };
